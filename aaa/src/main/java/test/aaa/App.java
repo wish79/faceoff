0609d53956e9a5c9f8dbd0d3484c97030454c2f6
wish79 <38513501+wish79@users.noreply.github.com>
2019-05-27 14:07:50 -0400

    Update App.java

--- aaa/src/main/java/test/aaa/App.java
+++ aaa/src/main/java/test/aaa/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! master111" );
+        System.out.println( "Hello World! master112222222221" );
     }
 }
