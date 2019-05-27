f8673505d4ee14f60ac52e1eab4ddd0584ad8efe
wish79 <38513501+wish79@users.noreply.github.com>
2019-05-27 14:07:50 -0400

    Update App.java

--- aaa/src/main/java/test/aaa/App.java
+++ aaa/src/main/java/test/aaa/App.java
@@ -8,9 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
+<<<<<<< Upstream, based on 1
 
         System.out.println( "Hello World!  11111" );
         System.out.println( "Hello World! master" );
 
+=======
+        System.out.println( "Hello World! master112222222221" );
+>>>>>>> 0609d53 Update App.java
     }
 }
