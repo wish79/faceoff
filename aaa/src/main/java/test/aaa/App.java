617c6f54da19c82387c023a708ccf50605a0ad88
weshahm <weshahm@USLTPC0JJY0A.mshome.net>
2019-05-27 14:40:40 -0400

    sss

--- aaa/src/main/java/test/aaa/App.java
+++ aaa/src/main/java/test/aaa/App.java
@@ -8,17 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
-<<<<<<< Upstream, based on 1
 
-        System.out.println( "Hello World!  11111" );
-        System.out.println( "Hello World! master" );
-
-=======
-        System.out.println( "Hello World! master112222222221" );
->>>>>>> 0609d53 Update App.java
-=======
-        System.out.println( "Hello World! master112222222221" );
->>>>>>> branch 'master' of https://github.com/wish79/faceoff.git
     }
 }
