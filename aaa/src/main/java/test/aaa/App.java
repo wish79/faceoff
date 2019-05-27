701cc3afecd8eb5d471be4b313e0f3cfc2d624c8
weshahm <weshahm@USLTPC0JJY0A.mshome.net>
2019-05-27 14:37:27 -0400

    commit

--- aaa/src/main/java/test/aaa/App.java
+++ aaa/src/main/java/test/aaa/App.java
@@ -8,7 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
+<<<<<<< Upstream, based on 1
         System.out.println( "Hello World!  11111" );
         System.out.println( "Hello World! master" );
+=======
+        System.out.println( "Hello World! master111" );
+>>>>>>> e78b502 Update App.java
     }
 }
